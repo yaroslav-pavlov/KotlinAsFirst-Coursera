fcb2fdcb030a292e6c371ed64e839700c66a5dc0
yaroslav pavlov <hh1199@gmail.com>
2018-10-24 17:18:32 +0300

    Lesson1.Task1 solved: trackLength thirdDigit travelMinutes numberRevert

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -54,19 +54,6 @@ fun main(args: Array<String>) {
 }
 
 /**
-class Tests {
-    @Test
-    fun TSeconds()
-    {
-        assertEquals(
-                30035,
-                seconds(8, 20, 35)
-        )
-    }
-}
- */
-
-/**
  * Тривиальная
  *
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
@@ -99,7 +86,7 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))
 
 /**
  * Простая
@@ -107,7 +94,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int = number/100%10
 
 /**
  * Простая
@@ -116,7 +103,7 @@ fun thirdDigit(number: Int): Int = TODO()
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = hoursArrive*60+minutesArrive-hoursDepart*60-minutesDepart
 
 /**
  * Простая
@@ -133,4 +120,4 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
+fun numberRevert(number: Int): Int = number%10*100+ number%100/10*10+number/100
